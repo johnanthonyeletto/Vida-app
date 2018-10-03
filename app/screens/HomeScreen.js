c9df8fb4bb7bef06de925fb2bc8909173ec13b7d
johnanthonyeletto <johnanthony.eletto@gmail.com>
2018-10-03 18:45:48 -0400

    Adding Models

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -17,7 +17,7 @@ export default class HomeScreen extends React.Component {
     title: 'Clients',
     headerTitle: <Image
     source={require('../assets/images/logo_1280x800.png')}
-    style={{height: 45, width: 72}}
+    style={{height: 45, width: 72, paddingTop: 5, paddingBottom: 5}}
   />,
   };
 
