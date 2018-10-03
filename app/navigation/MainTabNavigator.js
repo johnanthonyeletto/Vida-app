c1fb2b4398f3653ba1af093d974668f16ace94f4
johnanthonyeletto <johnanthony.eletto@gmail.com>
2018-10-03 15:26:45 -0400

    Updating tab navigation

--- app/navigation/MainTabNavigator.js
+++ app/navigation/MainTabNavigator.js
@@ -12,14 +12,14 @@ const HomeStack = createStackNavigator({
 });
 
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+  tabBarLabel: 'Clients',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
       name={
         Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
+          ? `ios-people${focused ? '' : '-outline'}`
+          : 'md-people'
       }
     />
   ),
