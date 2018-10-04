8ee1e8b890fd2551d49f509de4bcf192989fd741
johnanthonyeletto <johnanthony.eletto@gmail.com>
2018-10-04 00:25:29 -0400

    Setting up client list

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import {
   Image,
-  View,
+  ScrollView,
+  SectionList,
+  Text,
 } from 'react-native';
 import { WebBrowser } from 'expo';
+import ClientList from '../models/ClientList';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Clients',
     headerTitle: <Image
-    source={require('../assets/images/logo_1280x800.png')}
-    style={{height: 45, width: 72, paddingTop: 5, paddingBottom: 5}}
-  />,
+      source={require('../assets/images/logo_1280x800.png')}
+      style={{ height: 45, width: 72, paddingTop: 5, paddingBottom: 5 }}
+    />,
   };
+
+  constructor(props) {
+    super(props);
+
+    const clientList = new ClientList();
+    this.activeClients = clientList.getActive();
+    this.inactiveClients = clientList.getInactive();
+  }
 
   render() {
     return (
-      <View>
-
-      </View>
+      <ScrollView>
+        <SectionList
+          renderItem={({ item, index, section }) =>
+            <Text key={index}>{item.name}</Text>
+          }
+          renderSectionHeader={({ section: { title } }) => (
+            <Text style={{ fontWeight: 'bold' }}>{title}</Text>
+          )}
+          sections={[
+            { title: 'Active', data: this.activeClients },
+            { title: 'Inactive', data: this.inactiveClients },
+          ]}
+          keyExtractor={(item, index) => item + index}
+        />
+      </ScrollView>
     );
   }
 }
