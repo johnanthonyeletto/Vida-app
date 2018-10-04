7d7e1ebf693e8d05fa36690e3f010c6d088fc386
johnanthonyeletto <johnanthony.eletto@gmail.com>
2018-10-03 23:34:08 -0400

    Removing some useless shit

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import {
   Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
-
-import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
