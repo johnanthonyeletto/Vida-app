e5f34f4a22556557f8538addbcc207513e41f9fe
johnanthonyeletto <johnanthony.eletto@gmail.com>
2018-11-05 00:49:55 -0500

    get next meeting

--- api/app/Models/Person.php
+++ api/app/Models/Person.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Person extends Model
 {
@@ -11,12 +12,12 @@ class Person extends Model
 
     public function nextMeeting()
     {
-        return $this->hasMany('App\Models\Event', 'pid', 'pid')->latest();
+        return $this->hasMany('App\Models\Event', 'pid', 'pid')->where('event_datetime', '>', Carbon::now())->oldest();
     }
 
     public function relationships()
     {
-        // FIX THIS PLEASE TO INCLUDE PID1 AND PID2
+        // FIX THIS PLEASE TO INCLUDE BOTH PID1 AND PID2
         return $this->belongsToMany('App\Models\Person', 'relationships', 'client_id', 'pid1')->withPivot('relationshiptoclient');
     }
 }
