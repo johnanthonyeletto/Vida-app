a6e7899f9ae27efe202b144e0c6e91fcbcc475fa
johnanthonyeletto <johnanthony.eletto@gmail.com>
2018-11-07 08:56:29 -0500

    deleting app_old... Good riddance

--- app_old/Models/Client.js
+++
@@ -1,8 +0,0 @@
-export default class Client{
-    constructor(){
-        this.name = "John Eletto";
-        this.phone = "5164689725";
-        this.email = "johnanthony.eletto@gmail.com";
-        this.avatarURL = "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png";
-    }
-}
