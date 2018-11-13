b37b4846910990a671aeda838aef9d5aeebcb409
johnanthonyeletto <johnanthony.eletto@gmail.com>
2018-11-12 22:41:44 -0500

    Laying out add employee section

--- app/screens/Settings/ManageCompanyScreen.js
+++ app/screens/Settings/ManageCompanyScreen.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import Colors from '../../constants/Colors';
 import FormGroup from '../../components/forms/FormGroup';
 import ScrollContainer from '../../components/ScrollContainer';
+import ListSeparator from '../../components/ListSeparator';
 
 export default class ManageCompanyScreen extends Component {
     static navigationOptions = {
@@ -24,6 +25,9 @@ export default class ManageCompanyScreen extends Component {
     render() {
         return (
             <ScrollContainer>
+                <ListSeparator>
+                    <Text>Company Info</Text>
+                </ListSeparator>
                 <FormGroup
                     onChangeText={(name) => this.setState({ name })}
                     value={this.state.fname}
@@ -33,7 +37,56 @@ export default class ManageCompanyScreen extends Component {
                     autoCorrect={true}
                     maxLength={100}
                 />
+                <ListSeparator>
+                    <Text>Employees</Text>
+                </ListSeparator>
+                <View style={styles.addEmployeeContainer}>
+                    <TextInput
+                        style={styles.newEmployeeInput}
+                        onChangeText={(email) => this.setState({ email })}
+                        value={this.state.email}
+                        placeholder={"Add employee by email"}
+                        keyboardType={"email-address"}
+                        autoCapitalize={"none"}
+                        autoCorrect={false}
+                        textContentType={"emailAddress"}
+                        maxLength={100}
+                    />
+                    <TouchableOpacity onPress={() => { alert("Add Client") }} style={styles.addEmployeeButton}>
+                        <Text style={styles.addEmployeeButtonText}>Add</Text>
+                    </TouchableOpacity>
+                </View>
             </ScrollContainer>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    addEmployeeContainer: {
+        flexDirection: 'row'
+    },
+    newEmployeeInput: {
+        height: 50,
+        borderBottomColor: Colors.lightGrey,
+        borderBottomWidth: 1,
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 10,
+        marginRight: 10,
+        flexDirection: 'column',
+        flex: 1,
+    },
+    addEmployeeButton: {
+        backgroundColor: Colors.blue,
+        alignItems: 'center',
+        height: 50,
+        justifyContent: 'center',
+        flexDirection: 'column',
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+    addEmployeeButtonText: {
+        color: Colors.white,
+        fontSize: 15,
+    }
+});
